6fbf6f65265a9f0c6dd076325e6bc35cb7acd039
unknown <ARPGL005579$@persistent.co.in>
2022-12-08 18:17:55 +0530

    leetcode solution for jewel and stones

---
+++ script.js
@@ -0,0 +1,14 @@
+const jewelsInStones = (jewels,stones) =>
+{
+    let count=0;
+    for(let i=0;i<jewels.length;i++){
+        for(let j=0;j<stones.length;j++){
+            if (jewels.substring(0+i,i+1) === stones.substring(0+j,j+1))
+                count++;
+        }
+    } 
+    return `stone has ${count} jewels`;
+}
+const res = jewelsInStones('b','aAAbb');
+
+console.log(res);
